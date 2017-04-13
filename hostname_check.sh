7be53532979f2c6454e58d6382e5d932284f98e3
AllGray <mathiasthur@gmail.com>
2017-04-13 14:40:42 +0200

    Update hostname_check.sh

--- hostname_check.sh
+++ hostname_check.sh
@@ -12,20 +12,10 @@ sed -i "s/$hostname_old/$hostname_new/g" /etc/hostname
 sed -i "s/$hostname_old/$hostname_new/g" /etc/hosts
 
 
-# Finishing up
+# Finishing u
 echo "+---------------------------------------------------------------------+"
 echo "|                         Congratulation!                             |"
 echo "|                      Your install is done.                          |"
 echo "|                 Your HOSTNAME is $hostname_new                      |"
-echo "|           Head over to http://your.local.ip/owncloud                |"
-echo "|          If you don't have Bonjour/Netatalk installed               |"
-echo "|        Head over to http://$hostname_new.local/owncloud             |"
-echo "|            if you DO have Bonjour/Netatalk installed                |"
-echo "|                       To finish your setup                          |"
-echo "|                                                                     |"
-echo "| Username:     pick whatever you want                                |"
-echo "| Password:     pick whatever you want                                |"
-echo "| Mount folder: /media/owncloud/                                      |"
-echo "|                                                                     |"
 echo "|            This installer was brought to you by AllGray!            |"
 echo "+---------------------------------------------------------------------+"
