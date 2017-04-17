bed9875dfff22a69ed2e10a30d668cf38888deed
AllGray <mathiasthur@gmail.com>
2017-04-17 14:31:33 +0200

    Update testing_display.sh

--- testing_display.sh
+++ testing_display.sh
@@ -18,6 +18,20 @@ read -r hostname_old < /etc/hostname
 sed -i "s/$hostname_old/$hostname_new/g" /etc/hostname
 sed -i "s/$hostname_old/$hostname_new/g" /etc/hosts
 
+# Create Readme.txt in /user/chip
+cat >/user/chip/hostname_changer.txt <<EOL
+"+------------------------------------------------------+"
+"|                   Congratulation!!                   |"
+"|                 Your install is done                 |"
+"|              Your new HOSTNAME is $hostname_new      |"
+"|                                                      |"
+"|                                                      |"
+"|    This installer was brought to you by AllGray!!    |"
+"|              And the CHIPinstaller team              |"
+"+------------------------------------------------------+"
+EOL
+
+dialog --textbox /user/chip/hostname_changer.txt 22 70
 
 # Finishing u
 echo "+---------------------------------------------------------------------+"
