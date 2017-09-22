d911c87e6b5e3a0dab2fc41241a19786a9e8b3cb
AllGray <mathiasthur@gmail.com>
2017-09-22 18:52:19 +0200

    Update launcher.sh

--- terminalgame/launcher.sh
+++ terminalgame/launcher.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
+
 whiptail --title "TerminalGAME" --menu --separate-output "Choose:" 20 78 15 \
-"$P1" "Crawl" \
-"$P2" "Dopewars" \
-"$P3" "Empire" \
-"$P4" "Greed" \
-"$P5" "Mancala" \
-"$P6" "Moon Buggy" \
-"$P7" "NetHack Console" \
-"$P8" "Ninvaders" \
-"$P9" "Nsnake" \
-"$P10" "Slash 'em" \
-"$P11" "Tint" 2>games
+"$P1" "Crawl"  \
+"$P2" "Dopewars"  \
+"$P3" "Empire"  \
+"$P4" "Greed"  \
+"$P5" "Mancala"  \
+"$P6" "Moon Buggy"  \
+"$P7" "NetHack Console"  \
+"$P8" "Ninvaders"  \
+"$P9" "Nsnake"  \
+"$P10" "Slash 'em"  \
+"$P11" "Tint"  2>games
 
 CHOICE=$(cat games)
-case $CHOICE in
-      $P1) crawl
+case $CHOICE in		
+      P1) crawl
       ;;
-      $P2) dopewars
+      P2) dopewars
       ;;
-      $P3) empire
+      P3) empire
       ;;
-      $P4) greed
+      P4) greed
       ;;
-      $P5) mancala
+      P5) mancala
       ;;
-      $P6) moon-buggy
+      P6) moon-buggy
       ;;
-      $P7) nethack-console
+      P7) nethack-console
       ;;
-      $P8) ninvaders
+      P8) ninvaders
       ;;
-      $P9) nsnake
+      P9) nsnake
       ;;
-      $P10) slashem
+      P10) slashem
       ;;
-      $P11) tint
-
+      P11) tint
+      ;;
     esac
 done < games
