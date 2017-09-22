7d7ca843c530c457c21f91d41f49488985675657
AllGray <mathiasthur@gmail.com>
2017-09-22 16:53:59 +0200

    Update install.sh

--- terminalgame/install.sh
+++ terminalgame/install.sh
@@ -10,13 +10,13 @@ fi
 reset
 
 # Update
-apt-get update
+apt-get -y update
 
 # Install Dependencies
-apt-get install jq whiptail
+apt-get install -y jq whiptail
 
 # Install Games
-apt-get install angband bsdgames crawl dopewars empire greed mancala moon-buggy nethack-console ninvaders nsnake pacman4console ski slashem tint tome
+apt-get -y install angband bsdgames crawl dopewars empire greed mancala moon-buggy nethack-console ninvaders nsnake pacman4console ski slashem tint tome
 
 wget https://raw.githubusercontent.com/AllGray/NAME/master/launcher.sh
 chmod +x launcher.sh
